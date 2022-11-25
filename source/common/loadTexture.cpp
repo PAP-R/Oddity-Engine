360537bc67634c4d99f415966ffcbcb253775a2b
Jan Bischoff <jan.bisch@outlook.de>
2022-11-25 15:05:54 +0100

    Added glfw, glew, glm and images of Tim

--- source/common/loadTexture.cpp
+++ source/common/loadTexture.cpp
@@ -1,1 +1,50 @@
 #include "loadTexture.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdexcept>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+GLuint loadDDS(const char * imagepath) {
+    unsigned char header[124];
+
+    FILE *fp;
+
+    fp = fopen(imagepath, "rb");
+    if (fp == NULL) {
+        throw std::invalid_argument("Could not open dds file");
+    }
+
+    char filecode[4];
+    fread(filecode, 1, 4, fp);
+    if (strncmp(filecode, "DDS ", 4) != 0) {
+        fclose(fp);
+        throw std::runtime_error("File not of type dds");
+    }
+
+    fread(&header, 124, 1, fp);
+
+    unsigned int height = *(unsigned int*)&(header[8]);
+    unsigned int width = *(unsigned int*)&(header[12]);
+    unsigned int linearSize = *(unsigned int*)&(header[16]);
+    unsigned int mipMapCount = *(unsigned int*)&(header[24]);
+    unsigned int fourCC = *(unsigned int*)&(header[80]);
+
+    unsigned char * buffer;
+    unsigned int bufsize;
+
+    bufsize = mipMapCount > 1 ? linearSize * 2 : linearSize;
+    buffer = (unsigned char*) calloc(bufsize, sizeof(unsigned char));
+
+    fread(buffer, 1, bufsize, fp);
+    fclose(fp);
+
+    unsigned int components = (fourCC == FOURCC_DXT1) ? 3 : 4;
+}
