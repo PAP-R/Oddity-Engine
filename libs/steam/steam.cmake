f51f79645329c0579cef1c5fe41d59cd69b0e10b
Jan Bischoff <44364488+PAP-R@users.noreply.github.com>
2024-07-10 05:02:32 +0200

    Trying to fix the path

--- libs/steam/steam.cmake
+++ libs/steam/steam.cmake
@@ -1,37 +1,39 @@
+set(STEAM_DIRECTORY ${PROJECT_SOURCE_DIR}/libs/steam)
+
 include_directories(
         public/steam
 )
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
     set(STEAM_LIBRARIES
-            ${CMAKE_CURRENT_LIST_DIR}/redistributable_bin/linux64/libsteam_api.so
+            ${STEAM_DIRECTORY}/redistributable_bin/linux64/libsteam_api.so
     )
 
-    FILE(INSTALL ${CMAKE_CURRENT_LIST_DIR}/redistributable_bin/win64/steam_api64.dll DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    FILE(INSTALL ${STEAM_DIRECTORY}/redistributable_bin/win64/steam_api64.dll DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
     add_compile_definitions(ODDITY_ENGINE_STEAM)
     message("Steam for Linux")
 
 elseif (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     set(STEAM_LIBRARIES
-            ${CMAKE_CURRENT_LIST_DIR}/redistributable_bin/win64/steam_api64.lib
+            ${STEAM_DIRECTORY}/redistributable_bin/win64/steam_api64.lib
     )
 
-    FILE(INSTALL ${CMAKE_CURRENT_LIST_DIR}/redistributable_bin/win64/steam_api64.dll DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    FILE(INSTALL ${STEAM_DIRECTORY}/redistributable_bin/win64/steam_api64.dll DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
     add_compile_definitions(ODDITY_ENGINE_STEAM)
     message("Steam for Windows")
 
 elseif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set(STEAM_LIBRARIES
-            ${CMAKE_CURRENT_LIST_DIR}/redistributable_bin/osx/libsteam_api.dylib
+            ${STEAM_DIRECTORY}/redistributable_bin/osx/libsteam_api.dylib
     )
 
-    FILE(INSTALL ${CMAKE_CURRENT_LIST_DIR}/redistributable_bin/win64/steam_api64.dll DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    FILE(INSTALL ${STEAM_DIRECTORY}/redistributable_bin/win64/steam_api64.dll DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
     add_compile_definitions(ODDITY_ENGINE_STEAM)
     message("Steam for Mac")
 
 endif ()
 
-FILE(INSTALL ${CMAKE_CURRENT_LIST_DIR}/steam_appid.txt DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}) #TODO Remove at launch
+FILE(INSTALL ${STEAM_DIRECTORY}/steam_appid.txt DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}) #TODO Remove at launch
